f9e399b3067eeb6adf6bf54ab93ccd6620822c0c
M-Durmus <mdurmus9114@gmail.comm>
2021-12-29 09:45:28 +0300

    renk değişimi

--- src/App.js
+++ src/App.js
@@ -1,79 +1,97 @@
-import "./App.css";
-import { useState } from "react";
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
-import AddTask from "./components/AddTask";
-
+import './App.css';
+import Header from './components/Header';
+import {
+  useState, useEffect
+} from 'react';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+import axios from 'axios';
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Study React Pre-Class Notes",
-      day: "Dec 12th at 2:30pm",
-      isDone: false,
-    },
-    {
-      id: 2,
-      text: "Feed the Dog",
-      day: "Dec 13th at 1:30pm",
-      isDone: true,
-    },
-    {
-      id: 3,
-      text: "Attend In-Class",
-      day: "Dec 14th at 3:00pm",
-      isDone: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+  const [showAddTask, setShowAddTask] = useState(false)
+  const baseUrl = "http://localhost:5000/tasks";
+//Create Read Update Delete
+//Fetch tasks
+// const fetchTask= async()=>{
+//   const res = await fetch(baseUrl)
+//   const data =await res.json();
+//   console.log(data)};
+//Fetck Task with axios
+const fetchTask=async()=>{
+  // const res= await axios.get(baseUrl);
+  const {data}= await axios.get(baseUrl);
+  setTasks(data);
+};
+useEffect(()=>{
+  fetchTask()
+  }, [])
+  //Add Task
+  // 
+  const addTask = asycn(newTask) => {
+  const res = await fetch(baseUrl,{
+      method:"POST",
+      headers:{
+        "Content-type" : "application/json"
+        body: JSON.stringify(newTask)
+      }
 
-  const [showAddTask, setShowAddTask] = useState(false);
 
-  // ADD TASK
-  const addTask = (newTask) => {
-    // console.log("Add Task From App.js");
-    const id = Math.floor(Math.random() * 100) + 1;
-    const addNewTask = { id, ...newTask };
-    setTasks([...tasks, addNewTask]);
+    }fetchTask()
+   
   };
-
-  // DELETE TASK
+  //Delete Task
   const deleteTask = (deletedTaskId) => {
-    // console.log("delete", deletedTaskId);
-    setTasks(tasks.filter((task) => task.id !== deletedTaskId));
-  };
-
-  //TOGGLE DONE
+  console.log("delete", deletedTaskId);
+  setTasks(tasks.filter((task) => task.id !== deletedTaskId))
+}
+//Toggle Done
   const toggleDone = (toggleDoneId) => {
     setTasks(
-      tasks.map((task) =>
-        task.id === toggleDoneId ? { ...task, isDone: !task.isDone } : task
-      )
-    );
-  };
-
-  // SHOW ADD TASK
+      tasks.map((task) => task.id === toggleDoneId ? {
+        ...task,
+        isDone: !task.isDone
+      } : task)
+    )
+  }
+  //SHOW ADD TASK
   const toggleShow = () => setShowAddTask(!showAddTask);
-
-  // DELETE ALL TASKS
-  // TO DO FOR YOU
-
-  return (
-    <div className="container">
-      <Header
-        title="TASK TRACKER"
-        showAddTask={showAddTask}
-        toggleShow={toggleShow}
-      />
-
-      {showAddTask && <AddTask addTask={addTask} />}
-
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} deleteTask={deleteTask} toggleDone={toggleDone} />
-      ) : (
-        <p style={{ textAlign: "center" }}>NO TASK TO SHOW</p>
-      )}
-    </div>
-  );
-}
-
+  return ( <
+    div className = "container" >
+    <
+    Header title = "Task Tracker"
+    showAddTask = {
+      showAddTask
+    }
+    toggleShow = {
+      toggleShow
+    }
+    />
+    {
+      showAddTask && < AddTask addTask = {
+        addTask
+      }
+      />}
+      {
+        tasks.length > 0 ? ( < Tasks tasks = {
+            tasks
+          }
+          deleteTask = {
+            deleteTask
+          }
+          toggleDone = {
+            toggleDone
+          }
+          />
+          ):(
+            <p style = {{
+              textAlign: "center",
+              color: "gray"
+            }} > No Task To Show </p>
+        )} </div>
+    );
+  }
 export default App;
+
+
+
+
